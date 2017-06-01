c3c006ca4e57f1785b935cda23a77981fe9340cd
Rodrigo Freitas <rsfreitas@users.noreply.github.com>
2017-06-01 01:25:46 -0300

    Add support to user authentication and other options
    - Adding support to user authentication and the following options:
        - disable authentication;
        - create the default configuration file from an application;
        - create an empty database;
        - create an intermediate JTF file;
        - set database path;
        - set the session type.
    
    - Changing the getopt for the getopt_long.

--- src/main.c
+++ src/main.c
@@ -26,6 +26,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <getopt.h>
 
 #include <collections.h>
 #include <libxante.h>
@@ -36,15 +37,38 @@ static void usage(void)
 {
     printf("Usage: xanter [OPTIONS]\n");
     printf("A libxante application interpreter.\n\n");
-    printf("Options:\n");
-    printf(" -h\t\tShows this help screen.\n");
-    printf(" -v\t\tShows current xanter version.\n");
-    printf(" -j [filename]\tIndicates the JTF file pathname.\n");
-    printf(" -t [filename]\tChanges the UI theme.\n");
-    printf(" -u [username]\tUsername to run the application.\n");
-    printf(" -p [password]\tThe user password.\n");
-    printf(" -T\t\tPuts the interpreter into the test mode.\n");
-    printf(" -V\t\tShows current application version.\n");
+    printf("Options:\n\n");
+    printf("  -h, --help                 Shows this help screen.\n");
+    printf("  -v, --version              Shows current xanter version.\n");
+    printf("  -j [filename]              Indicates the JTF file pathname.\n");
+    printf("  -t [filename]              Changes the UI theme.\n");
+    printf("  -u [username]              Username to run the application.\n");
+    printf("  -p [password]              The user password.\n");
+    printf("  -T                         Puts the interpreter into the test mode.\n");
+    printf("  -V                         Shows current application version.\n");
+    printf("  -N                         Disables the user authentication.\n");
+    printf("  -C, --config [pathname]    Creates the application configuration file with\n\
+                                its default values, into JTF predefined path\n\
+                                or in the specified path.\n\n");
+
+    printf("  -d [path]                  Indicates the path to find the authentication\n\
+                                database.\n\n");
+
+    printf("  -D [path]                  Creates a database to use by applications.\n");
+    printf("  -J [filename]              Creates the JXDB file from an application.\n");
+    printf("  -S [session type]          Indicates the session type of the user login.\n");
+
+    printf("\n");
+    printf("Details:\n\n");
+    printf("Session types\n"
+           "\t%d -> A continuous login, which will last for every application "
+           "started\n\
+            after this one.\n"
+           "\t%d -> An individual login, which will cause a logout after "
+           "closing the\n\
+           application.\n", XANTE_SESSION_CONTINUOUS,
+           XANTE_SESSION_SINGLE);
+
     printf("\n");
 }
 
@@ -54,53 +78,125 @@ static void version(void)
            RELEASE, (BETA == true) ? "beta" : "");
 }
 
+static void application_version(xante_t *xpp)
+{
+    char *version_str = NULL;
+
+    version_str = xante_dlg_application_version(xpp);
+
+    if (version_str != NULL) {
+        printf("%s\n", version_str);
+        free(version_str);
+    }
+}
+
+static void error_msg(bool run_ui, const char *message)
+{
+    if (run_ui) {
+        xante_dlg_messagebox(NULL, XANTE_MSGBOX_ERROR, 0, cl_tr("Error"),
+                             "%s", message);
+    } else
+        printf("%s\n", message);
+}
+
+static void create_xante_database(const char *pathname)
+{
+    cl_init(NULL);
+
+    if (xante_auth_create_database(pathname, true))
+        error_msg(false, xante_strerror(xante_get_last_error()));
+
+    cl_uninit();
+}
+
 int main(int argc, char **argv)
 {
-    const char *opt = "hvj:Tt:u:p:V\0";
+    const char *opt = "hvj:Tt:u:p:VC::ND:J:d:S:\0";
+    struct option long_options[] = {
+        { "config",     required_argument, 0, 'C' },
+        { "help",       no_argument,       0, 'h' },
+        { "version",    no_argument,       0, 'v' },
+        { 0,            0,                 0, 0   }
+    };
+
     int option, ui_return = 0;
-    char *jtf_pathname = NULL, *theme_pathname = NULL, *username = NULL,
-         *password = NULL, *version_str = NULL;
-    bool use_plugin = true, show_application_version = false;
+    char *jtf_pathname = NULL, *username = NULL,
+         *password = NULL, *jxdb_pathname = NULL;
+    bool use_plugin = true, show_application_version = false, run_ui = true,
+         create_config_file = false, use_auth = true;
     xante_t *xpp = NULL;
+    enum xante_session session = XANTE_SESSION_SINGLE;
 
     do {
-        option = getopt(argc, argv, opt);
+        option = getopt_long(argc, argv, opt, long_options, NULL);
 
         switch (option) {
-            case 'h':
+            case 'h': /* usage */
                 usage();
                 return 1;
 
-            case 'v':
+            case 'v': /* version */
                 version();
                 return 2;
 
-            case 'j':
+            case 'j': /* JTF file */
                 jtf_pathname = strdup(optarg);
                 break;
 
-            case 'T':
-                /*
-                 * Test mode means the we're going to disable all plugin calls.
-                 */
+            case 'T': /* test mode */
+                /* Test mode means the we're going to disable all plugin calls. */
                 use_plugin = false;
                 break;
 
-            case 't':
-                theme_pathname = strdup(optarg);
+            case 't': /* theme file */
+                xante_env_set_theme(optarg);
                 break;
 
-            case 'u':
+            case 'u': /* username */
                 username = strdup(optarg);
                 break;
 
-            case 'p':
+            case 'p': /* password */
                 password = strdup(optarg);
                 break;
 
-            case 'V':
+            case 'V': /* application version */
                 show_application_version = true;
                 use_plugin = false;
+                break;
+
+            case 'C': /* create default configuration file */
+                create_config_file = true;
+                run_ui = false;
+                use_auth = false;
+                use_plugin = false;
+
+                if (optarg != NULL)
+                    xante_env_set_cfg_path(optarg);
+
+                break;
+
+            case 'N': /* disable database authentication */
+                use_auth = false;
+                break;
+
+            case 'D': /* create a default (empty) database */
+                create_xante_database(optarg);
+                return 3;
+
+            case 'J': /* create an intermediate JXDBI file */
+                jxdb_pathname = strdup(optarg);
+                run_ui = false;
+                use_plugin = false;
+                use_auth = false;
+                break;
+
+            case 'd': /* database path */
+                xante_env_set_auth_path(optarg);
+                break;
+
+            case 'S': /* session type */
+                session = atoi(optarg);
                 break;
 
             case '?':
@@ -110,57 +206,68 @@ int main(int argc, char **argv)
 
     /* TODO: This should be moved to the libxante library */
     cl_init(NULL);
-    xante_theme_set(theme_pathname);
 
     if (NULL == jtf_pathname) {
-        xante_dlg_messagebox(NULL, XANTE_MSGBOX_ERROR, 0, cl_tr("Error"),
-                             cl_tr("A JTF file name should be passed to the "
-                                   "interpreter."));
+        error_msg(run_ui, cl_tr("A JTF file name should be passed to the "
+                                "interpreter."));
 
         return -1;
     }
 
-    if ((NULL == username) || (NULL == password)) {
-        xante_dlg_messagebox(NULL, XANTE_MSGBOX_ERROR, 0, cl_tr("Error"),
-                             cl_tr("A username/password must be used to "
-                                   "run the application!"));
+    if (use_auth == true) {
+        if ((NULL == username) || (NULL == password)) {
+            error_msg(run_ui, cl_tr("A username/password must be used to "
+                                    "run the application!"));
 
-        return -1;
+            return -1;
+        }
     }
 
-    xpp = xante_init(jtf_pathname, use_plugin, username, password);
+    xpp = xante_init(jtf_pathname, use_plugin, use_auth, session, username,
+                     password);
 
     if (NULL == xpp) {
-        xante_dlg_messagebox(NULL, XANTE_MSGBOX_ERROR, 0, cl_tr("Error"),
-                             "%s", xante_strerror(xante_get_last_error()));
-
+        error_msg(run_ui, xante_strerror(xante_get_last_error()));
         return -1;
     }
 
     if (show_application_version == true) {
-        version_str = xante_dlg_application_version(xpp);
+        application_version(xpp);
+        goto end_block;
+    }
 
-        if (version_str != NULL) {
-            printf("%s\n", version_str);
-            free(version_str);
-        }
+    if (jxdb_pathname != NULL) {
+        if (xante_auth_export_jxdbi(xpp, jxdb_pathname) < 0)
+            error_msg(run_ui, xante_strerror(xante_get_last_error()));
 
         goto end_block;
     }
 
-    xante_load_config(xpp);
-    ui_return = xante_ui_run(xpp);
-    xante_write_config(xpp, ui_return);
+    if (create_config_file == true) {
+        /*
+         * We're going to just create the file. And to do so we force a few
+         * internal flags.
+         */
+        xante_runtime_set_force_config_file_saving(xpp, true);
+        xante_runtime_set_show_config_saving_question(xpp, false);
+    }
+
+    xante_config_load(xpp);
+
+    if (run_ui == true)
+        ui_return = xante_ui_run(xpp);
+
+    xante_config_write(xpp, ui_return);
 
 end_block:
     xante_uninit(xpp);
     cl_uninit();
 
+    if (jxdb_pathname != NULL)
+        free(jxdb_pathname);
+
     if (jtf_pathname != NULL)
         free(jtf_pathname);
-
-    if (theme_pathname != NULL)
-        free(theme_pathname);
 
     if (username != NULL)
         free(username);
